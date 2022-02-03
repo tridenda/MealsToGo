3d62e92bd9f7fdeb659f13070f4de78a3e9fde03
Tri Denda <34482931+tridenda@users.noreply.github.com>
2022-02-03 11:29:35 +0700

    add hello world

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text>Testing expo!</Text>
+      <Text>hello world!</Text>
     </View>
   );
 }
