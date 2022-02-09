4fc5d20e8fea17772d93e92a3dc0a0139bb5526f
Tri Denda <34482931+tridenda@users.noreply.github.com>
2022-02-09 22:50:34 +0700

    add function and map function

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components";
+
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+import { Search } from "./components/search.component";
 
 const Map = styled(MapView)`
   width: 100%;
   height: 100%;
 `;
 
-const MapContainer = styled.View`
-  flex: 1;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 20px;
+  width: 100%;
 `;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { viewport, lat, lng } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
-    <MapContainer>
-      <Map />
-    </MapContainer>
+    <>
+      <SearchContainer>
+        <Search />
+      </SearchContainer>
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurants) => {
+          return null;
+        })}
+      </Map>
+    </>
   );
 };
